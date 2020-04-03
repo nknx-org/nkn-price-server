421c015c13145f8a66827da0d3288a4898561f7a
Christian Busch <kontakt@christian-busch.com>
2020-04-03 15:51:18 +0200

    added some native casting for postgres

--- app/Price.php
+++ app/Price.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Price extends Model
 {
     protected $fillable = ['currency','price','volume_24h','percent_change_1h','percent_change_24h','percent_change_7d','market_cap'];
+    protected $casts = [
+        'price' => 'float',
+        'volume_24h' => 'float',
+        'percent_change_1h' => 'float',
+        'percent_change_24h' => 'float',
+        'percent_change_7d' => 'float',
+        'market_cap' => 'float'
+    ];
 
     public function quote()
     {
